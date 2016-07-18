2cae65d7ac66c1580aa607f83cfd8dfae1ea9e01
Bharath <bharath@riktamtech.com>
2016-07-18 16:31:41 +0530

    Initial Commit

---
+++ RTSliderView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RTSliderView
+//
+//  Created by Bharath Badam on 18/07/16.
+//  Copyright © 2016 Riktam Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
